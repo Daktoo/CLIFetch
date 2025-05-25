6309de608258c0e09c63e102a358c9f070225e13
Dakota7789 <dakotakearneycole@outlook.com>
2025-05-25 16:38:53 +0100

    script now downloads config file

--- clifetch.sh
+++ clifetch.sh
@@ -1,4 +1,13 @@
-# Welcome Message
+# Download config
+echo ' '
+echo 'Installing Config'
+echo ' '
+sleep 1
+cd ~
+wget https://raw.githubusercontent.com/Dakota7789/CLIFetch/refs/heads/main/config-cli.jsonc
+mv ~/config-cli.jsonc ~/.config/fastfetch/config-cli.jsonc
+
+# Welcome message
 echo ' '
 echo '---------------------------------------------------------------'
 echo ' '
