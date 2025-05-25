3672ccfe419575a7b1eca2f573ed331232cb9298
Dakota7789 <dakotakearneycole@outlook.com>
2025-05-25 16:22:22 +0100

    adding sh script

---
+++ clifetch.sh
@@ -0,0 +1,13 @@
+# Welcome Message
+echo ' '
+echo '---------------------------------------------------------------'
+echo ' '
+echo -e '\033[0mWelcome to \033[36mFastFetch \033[0mfor \033[31mCLI\033[0m!'
+echo ' '
+echo '---------------------------------------------------------------'
+echo ' '
+sleep 1
+
+# Fastfetch output
+fastfetch -c ~/.config/fastfetch/config-cli.jsonc
+
